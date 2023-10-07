33989f4765928eaab1d8202c6ed7ff8bf4890d7b
Anshul Chauhan <anshulchauhan@Anshuls-MacBook-Pro.local>
2023-10-07 19:42:52 +0530

    Shared Libs 6

--- vars/packageApplication.groovy
+++ vars/packageApplication.groovy
@@ -1,6 +1,8 @@
 def call() {
     node {
       dir('./java-tomcat-sample'){
+        sh 'ls'
+        sh 'pwd'
         sh '''
           mvn versions:set -DnewVersion="${VERSION_SUFFIX}"-SNAPSHOT
           mvn versions:update-child-modules
