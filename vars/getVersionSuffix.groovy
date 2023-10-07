db3a3c4fcaa0df44376ca7bfb0ede246372b3509
Anshul Chauhan <anshulchauhan@Anshuls-MacBook-Pro.local>
2023-10-07 19:40:50 +0530

    Shared Libs 6

--- vars/getVersionSuffix.groovy
+++ vars/getVersionSuffix.groovy
@@ -3,7 +3,7 @@ def call(Map config) {
     if (config.isReleaseCandidate) {
             return config.RELEASE_VER + ':' + env.BUILD_NUMBER
     } else {
-            return config.INT_VER + '+ci:' + env.BUILD_NUMBER
+            return config.INT_VER + 'ci:' + env.BUILD_NUMBER
         }
     }
 }
--- vars/packageApplication.groovy
+++ vars/packageApplication.groovy
@@ -1,9 +1,11 @@
 def call() {
     node {
+      dir('./java-tomcat-sample'){
         sh '''
-        mvn versions:set -DnewVersion="${VERSION_SUFFIX}"-SNAPSHOT
-        mvn versions:update-child-modules
-        mvn clean package
+          mvn versions:set -DnewVersion="${VERSION_SUFFIX}"-SNAPSHOT
+          mvn versions:update-child-modules
+          mvn clean package
       '''
     }
+  }
 }
