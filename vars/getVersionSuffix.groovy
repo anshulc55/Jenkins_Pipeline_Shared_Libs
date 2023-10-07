e89c9b01131467ef8830374ffb3d936d8ab7fdd7
Anshul Chauhan <anshulchauhan@Anshuls-MacBook-Pro.local>
2023-10-07 20:02:42 +0530

    Shared Libs 6

--- vars/getVersionSuffix.groovy
+++
@@ -1,9 +0,0 @@
-def call(Map config) {
-    node {
-    if (config.isReleaseCandidate) {
-            return config.RELEASE_VER + ':' + env.BUILD_NUMBER
-    } else {
-            return config.INT_VER + 'ci:' + env.BUILD_NUMBER
-        }
-    }
-}
